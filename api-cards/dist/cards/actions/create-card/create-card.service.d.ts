955a9c7fe0375a62637a844428c82560a93f07f6
jensyvillafranca <jensylorena2402@gmail.com>
2025-01-23 08:32:44 -0600

    Eliminar carpeta dist y node_modules del repositorio y actualizar .gitignore

--- api-cards/dist/cards/actions/create-card/create-card.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateCardDto } from '../create-card/create-card.dto';
-export declare class CreateCardService {
-    create(createCardDto: CreateCardDto, connection: any): Promise<{
-        idCard: any;
-        titleCard: string;
-        descriptions: string[];
-    }>;
-}
--- api-cards/dist/cards/actions/delete-card/delete-card.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class DeleteCardService {
-    remove(id: number, connection: any): Promise<{
-        message: string;
-    }>;
-}
