37df55ab5aa096891821673ee6cc08a5eec67c3b
jensyvillafranca <jensylorena2402@gmail.com>
2025-01-23 09:28:59 -0600

    Agregando archivo .env e ignorando la carpeta node_modules y dist en produccion

--- api-cards/src/app.module.ts
+++ api-cards/src/app.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CardModule } from './cards/card.module';
 import { LoggingService } from './logs/logging.service';
+import { DatabaseService } from './database/database.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ 
+      isGlobal: true 
+    }),
     CardModule,
   ],
   providers: [
     LoggingService, 
+    DatabaseService,
   ],
   exports: [
     LoggingService, 
+    DatabaseService,
   ],
 })
 export class AppModule {}
